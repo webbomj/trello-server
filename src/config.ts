7b8988b0ca959abdd14ec2870568e0d157027a33
Lapin Andrey <nethack@mail.ru>
2023-02-27 10:42:34 +0300

    chore: add dist

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-const port = 3000;
+const port = process.env.PORT;
 const urlDB = 'mongodb+srv://admin:v1VBsDSCDlkzGIKR@trello.id2jf8j.mongodb.net/trello?retryWrites=true&w=majority'
 export {urlDB, port}
