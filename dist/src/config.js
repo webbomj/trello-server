533347296cfdd06f5faec99ed19d95ea27664f2f
Lapin Andrey <nethack@mail.ru>
2023-02-27 14:13:22 +0300

    fix: create check item path

--- dist/src/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.port = exports.urlDB = void 0;
-var port = 3000;
-exports.port = port;
-var urlDB = 'mongodb+srv://admin:v1VBsDSCDlkzGIKR@trello.id2jf8j.mongodb.net/trello?retryWrites=true&w=majority';
-exports.urlDB = urlDB;
-//# sourceMappingURL=config.js.map
--- src/controllers/checkitem/createCheckItem.ts
+++ src/controllers/checkitem/createCheckItem.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express"
-import { createCheckItemService } from "../../services/checkItem/createCheckItem"
+import { createCheckItemService } from "../../services/checkitem/createCheckItem"
 
 export const createCheckItemController = async (req: Request, res: Response, next: NextFunction) => {
     try{
